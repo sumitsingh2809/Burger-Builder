76121fc5e53742a31259929180ee41827d5ad2a3
sumit singh <sumitsingh.lko95@gmail.com>
2020-08-19 02:22:59 +0530

    passing ingredients via query params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -120,7 +120,17 @@ class BurgerBuilder extends Component {
         //     .then((response) => this.setState({ loading: false, purchasing: false }))
         //     .catch((error) => this.setState({ loading: false, purchasing: false }));
 
-        this.props.history.push('/checkout');
+        const queryParams = [];
+        for (const i in this.state.ingredients) {
+            queryParams.push(
+                encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i])
+            );
+        }
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString,
+        });
     };
 
     render() {
