ade8639ecebb86a6a5efb2732a0fef39285f3581
sumit singh <sumitsingh.lko95@gmail.com>
2020-08-20 01:13:51 +0530

    outputting the orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -29,9 +29,9 @@ class Orders extends Component {
     render() {
         return (
             <div>
-                <Order />
-                <Order />
-                <Order />
+                {this.state.orders.map((order) => (
+                    <Order key={order.id} ingredients={order.ingredients} price={order.price} />
+                ))}
             </div>
         );
     }
