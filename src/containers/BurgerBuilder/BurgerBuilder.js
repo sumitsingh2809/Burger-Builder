895b2391cfb536eaeb144cc404196e882f0e5372
sumit singh <sumitsingh.lko95@gmail.com>
2020-08-24 01:33:37 +0530

    Adjusting checkout and contact data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false,
@@ -80,7 +79,7 @@ class BurgerBuilder extends Component {
             .map((igKey) => ingredients[igKey])
             .reduce((sum, el) => sum + el, 0);
 
-        this.setState({ purchasable: sum > 0 });
+        return sum > 0;
     };
 
     purchaseHandler = () => {
@@ -127,7 +126,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                         price={this.props.price}
                     />
