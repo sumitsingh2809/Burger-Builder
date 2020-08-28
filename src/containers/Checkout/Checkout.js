d54a134d594560115de99cf32436311b472a4ef6
sumit singh <sumitsingh.lko95@gmail.com>
2020-08-28 18:02:16 +0530

    fetching orders (via Redux)

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 
 import ContactData from './ContactData/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
     checkoutCancelledHandler = () => {
