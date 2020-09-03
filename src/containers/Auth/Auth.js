45fe30eff97e93085df1e26af04921d2b75b1df1
sumit singh <sumitsingh.lko95@gmail.com>
2020-09-04 01:46:53 +0530

    removed commented code

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -82,18 +82,6 @@ class Auth extends Component {
                 touched: true,
             }),
         });
-        // const updatedControls = {
-        //     ...this.state.controls,
-        //     [controlName]: {
-        //         ...this.state.controls[controlName],
-        //         value: event.target.value,
-        //         valid: this.checkValidity(
-        //             event.target.value,
-        //             this.state.controls[controlName].validation
-        //         ),
-        //         touched: true,
-        //     },
-        // };
 
         this.setState({ controls: updatedControls });
     };
