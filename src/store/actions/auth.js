aa1cb8404a84039b48c94de801d509a680ba460e
sumit singh <sumitsingh.lko95@gmail.com>
2020-09-04 16:24:25 +0530

    removing console.log()s

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -60,7 +60,6 @@ export const auth = (email, password, isSignUp) => {
         axios
             .post(url, authData)
             .then((response) => {
-                console.log(response);
                 localStorage.setItem('token', response.data.idToken);
                 localStorage.setItem('userId', response.data.localId);
                 localStorage.setItem(
